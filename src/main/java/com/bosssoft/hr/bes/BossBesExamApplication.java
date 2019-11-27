294da31ad5df14c58eb218e9ad4c9ff4911969c8
SsM479 <479372360@qq.com>
2019-11-27 19:47:06 +0800

    第一次提交：项目架构

---
+++ src/main/java/com/bosssoft/hr/bes/BossBesExamApplication.java
@@ -0,0 +1,17 @@
+package com.bosssoft.hr.bes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author��GuoShengWei
+ * @Description��
+ * @Date��Created in 2019/11/27 19:35
+ */
+@SpringBootApplication
+public class BossBesExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BossBesExamApplication.class, args);
+    }
+}
